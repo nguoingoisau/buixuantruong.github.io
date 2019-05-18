b73b29ed0c34743b4b801ae4d058751200ad9d06
BÙI XUÂN TRƯỜNG <truongbui.180799@gmail.com>
2019-05-18 11:55:34 +0700

    form and table

---
+++ dangkifb/fb.js
@@ -0,0 +1,43 @@
+
+
+let day = '<option>Ngày</option>';
+
+let month = '<option>Tháng</option>';
+
+let year = '<option>Năm</option>';
+
+
+
+for(let i=0;i<=31;i++){
+
+	day+= `<option>${i}</option>`;
+
+}
+
+
+
+for(let i=0;i<=12;i++){
+
+	month+= `<option>${i}</option>`;
+
+}
+
+
+
+for(let i=2019;i>=1905;i--){
+
+	year+= `<option>${i}</option>`;
+
+}
+
+
+
+document.getElementById('day').innerHTML=day;
+
+document.getElementById('month').innerHTML=month;
+
+document.getElementById('year').innerHTML=year;
+
+
+
+
